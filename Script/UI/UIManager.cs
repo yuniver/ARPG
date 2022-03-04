88bcb79da1ab3d3f89545eea7e77c8d457bd56be
yingliyu <terric.ying@coconutislandstudio.com>
2022-03-04 19:04:01 +0800

    code update

--- Script/UI/UIManager.cs
+++ Script/UI/UIManager.cs
@@ -10,13 +10,15 @@ public class UIManager : Singleton<UIManager>
     private Dictionary<string, string> _panelPaths = null;
     private FPContainer<PanelBase> _panelsCache = null;
     private Stack<PanelBase> _panelStack = null;
-
+    private GameObject _canvas;
     public UIManager() => Init();
 
     private void Init()
     {
         _panelPaths = getPathTable();
         _panelsCache = new FPContainer<PanelBase>(10);//最多缓存10个常用UI
+        _canvas = GameObject.Find("UICanvas");
+        _panelStack = new Stack<PanelBase>();
     }
 
     private Dictionary<string, string> getPathTable()
@@ -24,20 +26,45 @@ public class UIManager : Singleton<UIManager>
         TextAsset panels = LoadManager.Instance.Load<TextAsset>("UIPanels");
         var retVal = new Dictionary<string, string>();
 
-        return retVal;
+        throw new NotImplementedException();
     }
-
+    /// <summary>
+    /// 新界面入栈
+    /// </summary>
+    /// <param name="panelName">目标界面的名字</param>
     public void Push(string panelName)
     {
-        if (_panelStack == null)
-            _panelStack = new Stack<PanelBase>();
+        if (_panelStack.Count > 0)
+        {
+            var parentUI = _panelStack.Peek();
+            parentUI.OnPause();
+        }
 
-        PanelBase panel = GetPanel(panelName);
+        PanelBase panel = getPanel(panelName);
+        panel.OnEnter();
         _panelStack.Push(panel);
-
     }
-
-    private PanelBase GetPanel(string panelName)
+    /// <summary>
+    /// 退出界面（出栈）
+    /// </summary>
+    public void Pop()
+    {
+        if (_panelStack.Count == 0)
+            return;
+        var curPanel = _panelStack.Pop();
+        curPanel.OnExit(); //退出
+        if (_panelStack.Count != 0)
+        {
+            var parentPanel = _panelStack.Peek();
+            parentPanel.OnResume(); //恢复父级
+        }
+    }
+    /// <summary>
+    /// 获取面板对象
+    /// </summary>
+    /// <param name="panelName"></param>
+    /// <returns></returns>
+    private PanelBase getPanel(string panelName)
     {
         if (_panelPaths == null)
         {
